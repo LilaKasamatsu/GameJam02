b3bf0c7a55670b8d9a454c67f321d0b150a2a1de
Lila <kasamatsulila@gmail.com>
2020-06-15 14:31:05 +0200

    [added] environment changes color if you move over it
    (Nur in LilaTestScene, um keine Merge conflicts in der anderen Scene zu verursachen)

---
+++ Assets/Scripts/EnvironmentHealth.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnvironmentHealth
+{
+    public float maxHealth;
+    public float currentHealth;
+
+    public Color colorAlive;
+
+}
