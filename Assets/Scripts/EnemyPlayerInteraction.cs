bcb05d385ae6097086c06b54fb9878bcb7e01db5
Muschu <mascha.3rmakova@gmail.com>
2020-06-16 14:19:49 +0200

    [edited] fixed stuff and added an enemy yay

---
+++ Assets/Scripts/EnemyPlayerInteraction.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPlayerInteraction : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] bool sleeping;
+    [SerializeField] List<Vector3> location = new List<Vector3>();
+    [SerializeField] float speed;
+    Vector3 currLocation;
+    Transform newTransform;
+    Vector3 newLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //curr
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sleeping)
+        {
+           // Sleeping();
+        }
+        else
+        {
+          //  Moving();
+        }
+        
+    }
+    void Moving()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, newTransform.position,speed );
+        if (transform.position == newTransform.position)
+        {
+            Debug.Log("ssss");
+        }
+        //foreach (Vector3 element in location)
+        //{
+
+        //}
+
+    }
+    void SetNewLocation()
+    {
+        for (int i=0; i<location.Count; i++)
+        {
+            newLocation = location[i];
+            newTransform.position = newLocation;
+        }
+    }
+    void Sleeping()
+    {
+
+    }
+}
--- Assets/Scripts/SetNewRespwan.cs
+++ Assets/Scripts/SetNewRespwan.cs
@@ -6,12 +6,16 @@ public class SetNewRespwan : MonoBehaviour
 {
     Vector3 respawnLoc;
     Respwan respwanPlayer;
+    GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
         respawnLoc = GetComponent<Transform>().position;
         respwanPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Respwan>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        Debug.Log(player);
+        Debug.Log(respwanPlayer.location);
     }
     public void SetNewLocation()
     {
