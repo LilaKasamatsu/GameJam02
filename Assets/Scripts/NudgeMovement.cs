aa4cfb47d37f7ca93ebdac2ca353ac092d7a85f9
Lila <kasamatsulila@gmail.com>
2020-06-16 12:23:26 +0200

    [added] working nudging movement

--- Assets/Scripts/NudgeMovement.cs
+++ Assets/Scripts/NudgeMovement.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class NudgeMovement : MonoBehaviour
 {
+    #region Attributes
 
     public float pushForce = 3f;
 
     private Rigidbody rb;
+
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private Vector3 dir = Vector3.zero;
+
+    #endregion
 
     // Start is called before the first frame update
     void Start()
@@ -18,18 +25,39 @@ public class NudgeMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void OnMouseOver()
+
+
+
+    private void OnMouseEnter()
     {
+        startPosition = Input.mousePosition;
 
-        Vector3 dir = Input.mousePosition - transform.position;
+        //startPosition.z = -Camera.main.transform.position.z;
+        //startPosition = Camera.main.ScreenToWorldPoint(startPosition);       
+    }
 
-        dir = -dir.normalized;
 
-        rb.AddForce(dir * pushForce);
+    private void OnMouseExit()
+    {
+        endPosition = Input.mousePosition;
+        //endPosition.z = -Camera.main.transform.position.z;
+        //endPosition = Camera.main.ScreenToWorldPoint(endPosition);
 
-            
+
+        //Debug.Log(startPosition);
+        //Debug.Log(endPosition);
+
+        dir = endPosition - startPosition;
+
+
+        dir = dir.normalized;
+
+        dir = new Vector3(dir.x, 0, dir.y);
+        Debug.Log("Dir " + dir);
+
+        rb.AddForce(dir * pushForce, ForceMode.Impulse);
     }
 }
