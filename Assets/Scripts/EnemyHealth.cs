ac71e176c0d34d11a117bd053a47704b83462435
Muschu <mascha.3rmakova@gmail.com>
2020-06-23 14:44:29 +0200

    [edited] enemyradius

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -10,35 +10,66 @@ public class EnemyHealth : MonoBehaviour
     EnemyData enemy;
 
     [SerializeField] GameObject visualRadius;
-    private Color colorRadius;
+    private SpriteRenderer colorRadius;
     [SerializeField] Color aliveColor;
     [SerializeField] Color deadColor;
-    Color currColor;
-    int a = 100;
+    float a;
+    float b = 0.33f;
 
     // Start is called before the first frame update
     void Start()
     {
         enemyDeath = GetComponent<EnemyDeath>();
-        colorRadius = visualRadius.GetComponent<SpriteRenderer>().color;
-        colorRadius = aliveColor;
-        
+        colorRadius = visualRadius.GetComponent<SpriteRenderer>();
+        enemy = GetComponent<Enemy>().data;
+        colorRadius.color = aliveColor;
+        enemy.currHealth = enemy.maxHealth;
+      
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            MinusHealth(33);
+        }
     }
-   public void MinusHealth(int health)
+    public void MinusHealth(int health)
     {
-        enemy.currHealth -= enemy.currHealth;
-        colorRadius = Color.Lerp(aliveColor, deadColor, 0.33f);
-
+        enemy.currHealth -= health;
         if (enemy.currHealth <= 0)
         {
-            Debug.Log("enemy Death");
+           // Debug.Log("enemy Death");
             enemyDeath.Kill();
         }
+        else
+        {
+
+            StartCoroutine(MinusHealthCoroutine());
+
+        }
+    }
+    IEnumerator MinusHealthCoroutine()
+    {
+        
+
+        while (true)
+        {
+            Debug.Log("ee");
+            colorRadius.color = Color.Lerp(aliveColor, deadColor, a);
+            a += 0.01f;
+            if (a >= b)
+            {
+                Debug.Log(a);
+                b += 0.33f;
+                break;
+
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        yield return null;
+
     }
 }
