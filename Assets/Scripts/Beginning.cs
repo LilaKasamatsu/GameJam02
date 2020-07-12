dd86b22bd304a391d3cc410360eaf00f3e8feb6c
Meike <meike.strippel@gmail.com>
2020-07-12 18:22:19 +0200

    [added] beginning scene + scripts

---
+++ Assets/Scripts/Beginning.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beginning : MonoBehaviour
+{
+    [SerializeField] GameObject fadeOut;
+    [SerializeField] GameObject black;
+    [SerializeField] GameObject one;
+    [SerializeField] GameObject two;
+    [SerializeField] GameObject three;
+    [SerializeField] GameObject four;
+    [SerializeField] GameObject five;
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(FadeOut());
+        //StartCoroutine(Words());
+    }
+
+    IEnumerator FadeOut()
+    {
+        yield return new WaitForSecondsRealtime(17f);
+        fadeOut.SetActive(true);
+    }
+
+    IEnumerator Words()
+    {
+        yield return new WaitForSecondsRealtime(4f);
+        black.SetActive(true);
+        one.SetActive(true);
+        yield return new WaitForSecondsRealtime(4f);
+        black.SetActive(false);
+
+    }
+}
